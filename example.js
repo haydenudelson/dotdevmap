967909afa6e14a26bd8c768c3b6088e3ae0a5fc0
Hayden Udelson <haydenudelson18@gmail.com>
2018-04-29 18:31:31 -0600

    Map all data points onto map

--- example.js
+++ example.js
@@ -1,43 +1,19 @@
-var myLatLng = [{lat: 41.8781, lng: -87.6298},
-				{lat: 41.932, lng: -87.243},
-				{lat: 42.00, lng: -87.5},
-				{lat: 41.89, lng: -87.5}]
-
-
+var westcampus = [{'name':'Google West Campus 1','lat':41.86,'lng':-87.7}]
 
 function myMap() {
-  var mapCenter = new google.maps.LatLng(41.8781,-87.6298);
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: mapCenter, zoom: 11};
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  //map.data.loadGeoJson('crimespastyear.json')
-  map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json');
-  for (i = 0; i <= myLatLng.length; i++) {
-  	plotPoint(map, myLatLng[i]);
-  }
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 11,
+		center: {lat: 41.8781, lng: -87.6298},
+	});
+
+	for (x in testing) {
+		var building = testing[x];
+		var location = new google.maps.LatLng(building.lat,building.lng);
+		var marker = new google.maps.Marker({
+			position: location,
+			title: "Crime",
+			map: map
+		});
+	}
 }
-
-function plotData() {
-	var data;
-
-	Papa.parse('File:///D:/Hayden/Files/Northwestern/dot dev/formatteddata.csv', {
-		header: true,
-		download: true,
-
-		complete: function(results) {
-			console.log(results);
-			data = results.data;
-  }
-});
-	var element = document.getElementById("test");
-	element.innerHTML = data;
-}
-
-function plotPoint(map, data) {
-	var marker = new google.maps.Marker({
-		position: data,
-		map: map
-	})
-}
-
-plotData()
+google.maps.event.addDomListener(window, 'load', myMap);
